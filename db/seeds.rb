598b047d9b8f1bbdbd4943c54db559b0b56fa67f
Kevin Kim <khk271@nyu.edu>
2020-04-06 16:59:26 -0400

    completed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+new_cocktail = Cocktail.new(name: "Gin & Tonic")
+new_dose = Dose.new(description: "1 shot")
+new_dose.ingredient = Ingredient.find(3)
+new_dose.cocktail = new_cocktail
+new_dose.save
+new_cocktail.save
