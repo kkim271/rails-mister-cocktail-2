9042ca3014e1385ab77851a18357911363cbd19d
Kevin Kim <khk271@nyu.edu>
2020-04-06 18:10:08 -0400

    created cocktail and dose controllers, defined routes for cocktails and doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :cocktail, only: [:index, :show, :new, :create, :destroy] do
+    resources :dose, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
