1b9263cd882aa2cad1778dd63c7e39177fdd469c
Kevin Kim <khk271@nyu.edu>
2020-04-06 22:21:43 -0400

    added dose list and add dose function to show view

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,16 +1,36 @@
+require 'byebug'
+
 class CocktailsController < ApplicationController
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+    @dose = Dose.new
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.create(cocktail_params)
+
+    redirect_to cocktails_path
   end
 
   def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,7 +1,24 @@
+require 'byebug'
+
 class DosesController < ApplicationController
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.create(
+      description: params[:dose][:description],
+      cocktail: @cocktail,
+      ingredient_id: params[:dose][:ingredient_id]
+    )
+    redirect_to cocktail_path(params[:cocktail_id])
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
   end
 end
