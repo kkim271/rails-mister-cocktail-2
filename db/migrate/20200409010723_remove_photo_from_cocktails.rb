49bb0ae5faaafded2daade56d28e5ceabe7bf9c7
Kevin Kim <khk271@nyu.edu>
2020-04-08 22:08:15 -0400

    reverted incorrect changes to images, functioning version

--- db/migrate/20200409010723_remove_photo_from_cocktails.rb
+++
@@ -1,6 +0,0 @@
-class RemovePhotoFromCocktails < ActiveRecord::Migration[6.0]
-  def change
-
-    remove_column :cocktails, :photo, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_010723) do
+ActiveRecord::Schema.define(version: 2020_04_08_225029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,15 +36,11 @@ ActiveRecord::Schema.define(version: 2020_04_09_010723) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "articles", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo"
   end
 
   create_table "doses", force: :cascade do |t|
