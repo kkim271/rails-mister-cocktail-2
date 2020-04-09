7261ec7bc467f790d9117e648b57e40ab8e00f3b
Kevin Kim <khk271@nyu.edu>
2020-04-08 21:08:29 -0400

    removed photo from Cocktail model

---
+++ db/migrate/20200409010723_remove_photo_from_cocktails.rb
@@ -0,0 +1,6 @@
+class RemovePhotoFromCocktails < ActiveRecord::Migration[6.0]
+  def change
+
+    remove_column :cocktails, :photo, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_225855) do
+ActiveRecord::Schema.define(version: 2020_04_09_010723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 2020_04_08_225855) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "photo"
   end
 
   create_table "doses", force: :cascade do |t|
